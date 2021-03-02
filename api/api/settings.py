1c3098f7919b98c1e89a622865670c0b53322dac
Andrey Terekhov <shizdryeah@gmail.com>
2021-03-02 13:43:10 +0300

    02.03.2021

--- api/api/settings.py
+++ api/api/settings.py
@@ -177,4 +177,12 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.DjangoModelPermissions",),
+    "SEARCH_PARAM": "q",
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'Ваша почта'
+EMAIL_HOST_PASSWORD = 'Пароль который вы только тчо получили'
